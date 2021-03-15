26746decd2c1575bc1c6a886bdc7e41b879261ad
atharva-librax <79016996+atharva-librax@users.noreply.github.com>
2021-03-15 12:06:41 -0400

    Updated ID Verification example code

--- ID-verification-example.py
+++ ID-verification-example.py
@@ -1,8 +1,15 @@
+"""
+    Tutorial can be found at https://github.com/LibraX-ai/IDVerification
+"""
 import os, requests, base64, json
 import requests
+
 subscription_key = "Get Your Key at Dev.LibraX.ai"
 assert subscription_key
+
+# Path of the ID image you wish to test ID verification on
 FilePath = 'Your ID Photo'
+
 MainURL = "https://api.librax.ai"
 TargetURL  = MainURL+"/id/verify"
 
@@ -16,16 +23,12 @@ image_file = open(FilePath, "rb")
 with open(FilePath, "rb") as image_file:
     encoded_string  = base64.b64encode(image_file.read()).decode(ENCODING) 
 
-
-base64_files = json.dumps({
+# FirstName, LastName and ID Photo should be present in the request body
+payload_data = json.dumps({
     "IDPhoto": encoded_string,
     "LastName": "Smith",
     "FirstName": "Adam"
 })
 
-response = requests.request("POST", url=TargetURL, data=base64_files, headers=HEADER, stream=True)
+response = requests.request("POST", url=TargetURL, data=payload_data, headers=HEADER, stream=True)
 response.json()
-
-
-
-
